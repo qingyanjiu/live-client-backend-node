303151f08667390489aa61c96911c24155109037
louisliu <louisliu@cisco.com>
2018-07-05 11:12:12 +0800

    cors support

--- app.js
+++ app.js
@@ -49,6 +49,21 @@ app.use(function (req, res, next) {
 app.use('/live', liveRoutes);
 
 
+//allow custom header and CORS
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); /让options请求快速返回/
+    }
+    else {
+        next();
+    }
+});
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
