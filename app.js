b745406be16879e138bd0aac85fd59a42373863a
louisliu <louisliu@cisco.com>
2018-07-05 15:18:04 +0800

    update auth0 jwt and cors

--- app.js
+++ app.js
@@ -9,22 +9,19 @@ var liveRoutes = require('./routes/Live');
 
 var app = express();
 
-var jwt = require('express-jwt');
-var jwks = require('jwks-rsa');
+//allow custom header and CORS
+app.all('*',function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: "https://mokulive.auth0.com/.well-known/jwks.json"
-    }),
-    audience: 'http://64.137.224.204:5000/',
-    issuer: "https://mokulive.auth0.com/",
-    algorithms: ['RS256']
+    if (req.method == 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 });
-
-app.use(jwtCheck);
 
 app.get('/authorized', function (req, res) {
     res.send('Secured Resource');
@@ -43,6 +40,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use(flash());
 
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://mokulive.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'http://64.137.224.204:5000/',
+    issuer: "https://mokulive.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 app.use(function (req, res, next) {
 
@@ -68,22 +81,6 @@ app.use(function (req, res, next) {
 
 
 app.use('/live', liveRoutes);
-
-
-//allow custom header and CORS
-app.all('*',function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-    if (req.method == 'OPTIONS') {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-});
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
