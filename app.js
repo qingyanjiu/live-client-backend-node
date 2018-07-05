022f2fb0069a538f8391bf594ce287df93b5aa42
louisliu <louisliu@cisco.com>
2018-07-05 13:47:44 +0800

    added auth0 jwt

--- app.js
+++ app.js
@@ -9,6 +9,27 @@ var liveRoutes = require('./routes/Live');
 
 var app = express();
 
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: "https://mokulive.auth0.com/.well-known/jwks.json"
+    }),
+    audience: 'http://64.137.224.204:5000/',
+    issuer: "https://mokulive.auth0.com/",
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
+app.get('/authorized', function (req, res) {
+    res.send('Secured Resource');
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
